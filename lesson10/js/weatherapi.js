08433aaacabfa261844e51ad1bfd187d16a5c992
YuliaGT <tanner141517@gmail.com>
2021-03-10 13:58:19 -0700

    API test

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c47e55d54eb3cb601a2de0b71c34823d";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c47e55d54eb3cb601a2de0b71c34823d";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
