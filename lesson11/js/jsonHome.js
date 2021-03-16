93c6238f77b3db9319ce151ba1a56af5ca4506db
YuliaGT <tanner141517@gmail.com>
2021-03-15 22:02:55 -0600

    week11

---
+++ lesson11/js/jsonHome.js
@@ -0,0 +1,55 @@
+
+/*----------- JSON --------------*/
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject["towns"];
+    /* console.log(towns); 
+   console.log(typeof(towns)); */
+    const maintowns = document.querySelector(".maintowns");
+
+    const townsfilter = towns.filter(
+      (town) =>
+        town.name === "Preston" ||
+        town.name === "Soda Springs" ||
+        town.name === "Fish Haven"
+    );
+
+
+    //for (let i = 0; i < towns.length; i++ ) {
+    townsfilter.forEach((town) => {
+      let card = document.createElement("section");
+      let info = document.createElement("div");
+      let h3 = document.createElement("h3");
+      let h4 = document.createElement("h4");
+      let h5 = document.createElement("h5");
+      let h52 = document.createElement("h5");
+      let h53 = document.createElement("h5");
+      let img = document.createElement("img");
+      
+
+      h3.innerHTML = `${town.name}`;
+      h4.innerHTML = `${town.motto}`;
+      h5.innerHTML = "Year Founded: " + `${town.yearFounded}`;
+      h52.innerHTML = "Population: " + `${town.currentPopulation}`;
+      h53.innerHTML = "Annual Rain Fall: " + `${town.averageRainfall}`;
+      img.setAttribute("src", `images/${town.photo}`);
+      img.setAttribute("alt", `${town.name}` + " photo");
+
+      info.appendChild(h3);
+      info.appendChild(h4);
+      info.appendChild(h5);
+      info.appendChild(h52);
+      info.appendChild(h53);
+      card.appendChild(info);
+      card.appendChild(img);
+      
+
+      document.querySelector("div.maintowns").appendChild(card);
+    });
+  });
---
+++ lesson11/js/weatherPres.js
@@ -0,0 +1,58 @@
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=c47e55d54eb3cb601a2de0b71c34823d";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById("currentlyvalue").textContent =
+      jsObject.weather[0].main;
+    document.getElementById("tempvalue").textContent = Math.round(jsObject.main.temp);
+    document.getElementById("speedvalue").textContent = jsObject.wind.speed;
+    document.getElementById("humidityvalue").textContent =
+      jsObject.main.humidity;
+
+    if (jsObject.main.temp > 50 && jsObject.wind.speed < 3) {
+      document.getElementById("chillvalue").textContent = "N/A";
+    } else {
+      document.getElementById("chillvalue").textContent =
+        35.74 +
+        0.6215 * jsObject.main.temp -
+        35.75 * Math.pow(jsObject.wind.speed, 0.16) +
+        0.4275 * jsObject.main.temp * Math.pow(jsObject.wind.speed, 0.16);
+      document.getElementById("chillvalue").textContent =
+        Math.round(document.getElementById("chillvalue").textContent * 10) / 10;
+    }
+  });
+
+// 5 day forecast
+
+const forecastapiURL =
+  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=c47e55d54eb3cb601a2de0b71c34823d";
+fetch(forecastapiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let dayCount = 0;
+    const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    //filter
+    const tempfilter = jsObject.list.filter((x) =>
+      x.dt_txt.includes("18:00:00")
+    );
+
+    tempfilter.forEach((day) => {
+      let d = new Date(day.dt_txt);
+      document.getElementById(`forcastday${dayCount + 1}`).textContent =
+        dayofWeek[d.getDay()];
+      document.getElementById(`temp${dayCount + 1}`).textContent =
+      Math.round(day.main.temp);
+      let imagesrc = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+      document
+        .getElementById(`icon${dayCount + 1}`)
+        .setAttribute("src", imagesrc);
+      document
+        .getElementById(`icon${dayCount + 1}`)
+        .setAttribute("alt", day.weather[0].description);
+      dayCount++;
+    });
+  });
