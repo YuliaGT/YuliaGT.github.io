4506253bc61e66f8198ecded84b11f137cb76459
YuliaGT <tanner141517@gmail.com>
2021-03-26 22:57:35 -0600

    week 12

---
+++ chamber/js/forecastSpanish.js
@@ -0,0 +1,30 @@
+const forecastsapiURL =
+  "https://api.openweathermap.org/data/2.5/forecast?id=5781860&units=imperial&appid=c47e55d54eb3cb601a2de0b71c34823d";
+fetch(forecastsapiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let dayCount = 0;
+    const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    //filter
+    const tempfilter = jsObject.list.filter((x) =>
+      x.dt_txt.includes("18:00:00")
+    );
+
+    tempfilter.forEach((day) => {
+      let d = new Date(day.dt_txt);
+      document.getElementById(`forcastdays${dayCount + 1}`).textContent =
+        dayofWeek[d.getDay()];
+      document.getElementById(`temps${dayCount + 1}`).textContent =
+      Math.round(day.main.temp);
+      let imagesrc = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+      document
+        .getElementById(`icons${dayCount + 1}`)
+        .setAttribute("src", imagesrc);
+      document
+        .getElementById(`icons${dayCount + 1}`)
+        .setAttribute("alt", day.weather[0].description);
+      dayCount++;
+    });
+  });
