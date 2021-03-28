d5372111891cf03813d152f982a472eb5bcb0b01
YuliaGT <tanner141517@gmail.com>
2021-03-28 15:18:56 -0600

    week12

---
+++ chamber/js/directors.js
@@ -0,0 +1,72 @@
+const requestURL = 'https://yuliagt.github.io/chamber/directors.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+   // console.table(jsonObject); 
+    const prophets = jsonObject['directors']; 
+    // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let img = document.createElement('img');
+       
+        
+        
+        h2.textContent = directors[i].name;
+        p.textContent = directors[i].director;
+        p2.textContent =directors[i].company;
+        img.setAttribute('src', prophets[i].imageurl);
+        img.setAttribute('alt', prophets[i].name+ prophets[i].lastname  +"-"+ prophets[i].order );
+   
+        
+        card.appendChild(h2);
+        card.appendChild(p);
+        card.appendChild(p2);
+        card.appendChild(img);
+
+
+        document.querySelector('div.cards').appendChild(card); 
+        
+    }
+    
+        
+  });
+
+
+  
+// Lazy load
+
+  const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: .5,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+        imagesToLoad.forEach((img) => {
+            imgObserver.observe(img);
+        });
+} else{
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+});
+}
